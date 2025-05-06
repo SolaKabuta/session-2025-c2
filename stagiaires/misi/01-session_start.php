ea2cb185158ad8b92df4c96739e5d6460cd415c3
Daniel Misi <misi@MacBook-Air-de-Daniel.local>
2025-05-06 14:22:04 +0200

    stagiaire test

---
+++ stagiaires/misi/01-session_start.php
@@ -0,0 +1,16 @@
+<?php
+
+// creation du cookie sur le navigateur de l'utilisateur
+session_start();
+
+if(empty($_SESSION["ConnectUnixTime"])){
+    // echo "sa marche";
+    $_SESSION["ConnectUnixTime"] = time();
+    $_SESSION['countView'] = 1;
+}
+
+// variable global de type $_SESSION 
+var_dump($_SESSION);
+
+echo "<br>" . $_SESSION['CountView']++ .' vues ';
+echo 'depuis ' . ( time() - $_SESSION["ConnectUnixTime"]) . "secondes";
