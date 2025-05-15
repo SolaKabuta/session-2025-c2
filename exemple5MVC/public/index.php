b48c7a92b939ac5037f52ca4acb447c40b1319bb
WebDevCF2m <gitweb@cf2m.be>
2025-05-15 15:18:46 +0200

    first links

--- exemple5MVC/public/index.php
+++ exemple5MVC/public/index.php
@@ -5,5 +5,22 @@
  * Contrôleur frontal
  */
 
+
+// création de la session (ou continuation)
+// crée un cookie nommé PHPSESSID côté utilisateur
+// crée un fichier texte lié côté serveur
+session_start();
+
 // dépendances
 require_once "../config-dev.php";
+
+
+// chargement d'une vue
+include "../view/homepage.view.html.php";
+
+echo '<h3>$_GET</h3>';
+var_dump($_GET);
+echo '<h3>$_SESSION</h3>';
+var_dump($_SESSION);
+echo '<h3>$_POST</h3>';
+var_dump($_POST);
